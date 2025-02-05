881c51079628b70d5f5254f869ebda209e51c307
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-02-05 10:05:24 -0600

    Update radixSort.java

--- radixSort.java
+++ radixSort.java
@@ -1,1 +1,41 @@
+public class RadixSort implements IGenericSort<Integer> {
+    @Override
+    public void sort(Integer[] array) {
+        int max = getMax(array);
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(array, exp);
+        }
+    }
+
+    private int getMax(Integer[] array) {
+        int max = array[0];
+        for (int i : array) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    private void countingSort(Integer[] array, int exp) {
+        int n = array.length;
+        Integer[] output = new Integer[n];
+        int[] count = new int[10];
+        
+        for (int i = 0; i < n; i++) {
+            count[(array[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(array[i] / exp) % 10] - 1] = array[i];
+            count[(array[i] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, array, 0, n);
+    }
+}
 
